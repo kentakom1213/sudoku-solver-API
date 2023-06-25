c7c31796eef5974967338cf578c093b98093921e
kentakom1213 <kentakom1213@gmail.com>
2023-06-26 02:05:54 +0900

    [fix] マージのミスを修正

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,7 @@ use axum::{
     routing::{get, post},
     Router,
 };
-use std::net::SocketAddr;
 use sudoku::solver;
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
@@ -16,19 +14,5 @@ async fn axum() -> shuttle_axum::ShuttleAxum {
         .route("/", get(hello_world))
         .route("/solve", post(solver::solve));
 
-    // アプリケーションの定義
-    let app = Router::new()
-        .route("/", get(hello_world))
-        .route("/solve", post(solver::solve));
-
-    // 実行
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-
-    // デバッグログ
-    tracing::debug!("listening on {}", addr);
-
-    axum::Server::bind(&addr)
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+    Ok(router.into())
 }
