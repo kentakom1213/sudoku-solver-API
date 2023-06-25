c8ab86b68e633c08d73db48ab5c2f2a121e86481
kentakom1213 <kentakom1213@gmail.com>
2023-06-26 01:48:18 +0900

    [add] Jsonのやり取りに成功

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,37 @@ use axum::{
     routing::{get, post},
     Router,
 };
+use std::net::SocketAddr;
 use sudoku::solver;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
 }
 
-#[shuttle_runtime::main]
-async fn axum() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new()
-        .route("/", get(hello_world));
-        // .route("/solve", post(solver::solve));
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "example_templates=debug".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
 
-    Ok(router.into())
+    // アプリケーションの定義
+    let app = Router::new()
+        .route("/", get(hello_world))
+        .route("/solve", post(solver::solve));
+
+    // 実行
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    // デバッグログ
+    tracing::debug!("listening on {}", addr);
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
