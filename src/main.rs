baa968dcfb72155c885269bc051359f32fc1d684
kentakom1213 <kentakom1213@gmail.com>
2023-06-30 16:51:21 +0900

    [fix] とりあえずCORSをすべて許可

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use axum::{
     Router,
 };
 use sudoku::solver;
+use tower_http::cors::{Any, CorsLayer};
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
@@ -10,9 +11,16 @@ async fn hello_world() -> &'static str {
 
 #[shuttle_runtime::main]
 async fn axum() -> shuttle_axum::ShuttleAxum {
+    // CORSを許可
+    let cors = CorsLayer::new()
+        .allow_methods(Any)
+        .allow_origin(Any)
+        .allow_headers(Any);
+
     let router = Router::new()
         .route("/", get(hello_world))
-        .route("/solve", post(solver::solve));
+        .route("/solve", post(solver::solve))
+        .layer(cors);
 
     Ok(router.into())
 }
