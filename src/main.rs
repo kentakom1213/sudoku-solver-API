bf77de60fec67e8a5ce10d2bb6dc9687b6e2041b
kentakom1213 <kentakom1213@gmail.com>
2023-07-01 00:36:44 +0900

    [fix] フォーマット

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use axum::{
     routing::{get, post},
     Router,
 };
-use http::Method;
 use sudoku::solver;
 use tower_http::cors::{Any, CorsLayer};
 
@@ -13,10 +12,7 @@ async fn hello_world() -> &'static str {
 #[shuttle_runtime::main]
 async fn axum() -> shuttle_axum::ShuttleAxum {
     // corsを許可
-    let cors = CorsLayer::new()
-        .allow_headers(Any)
-        .allow_origin(Any);
-        // .allow_methods([Method::GET, Method::POST]);
+    let cors = CorsLayer::new().allow_headers(Any).allow_origin(Any);
 
     let router = Router::new()
         .route("/", get(hello_world))
